ad6af51eb59bc0d0642b7ce647573200bea5608c
chof747 <chof@gmx.at>
2020-08-06 07:03:17 +0200

    Moved config.h, config.sample.h to include folder

---
+++ firmware/include/config_sample.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+//WIFI Parameters
+#define WIFI_SSID                             "WIFI_ID"
+#define WIFI_PASSWORD                         "WIFI_PASSWORD"
+
+//MQTT Parameters
+#define MQTT_SERVER                           "MQTT_SERVER"
+#define MQTT_USER                             "MQTT_USER"
+#define MQTT_PASSWORD                         "MQTT_PASSWORD"
+
+
+#endif //config.h
