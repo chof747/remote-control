cc89d725ecbeadde79b8a9077afe2a3ad60640d5
chof747 <chof@gmx.at>
2021-11-16 20:32:25 +0100

    moving old stuff out of the way

---
+++ firmware_v1/ota/dev_config.py
@@ -0,0 +1,3 @@
+OTA_SERVER = "localhost"
+OTA_USER   = "chof"
+OTA_PATH   = "~/Documents/projects/homeautomation/homecenter/homeassistent/test/docker/firmware"
---
+++ firmware_v1/ota/prod_config.py
@@ -0,0 +1,3 @@
+OTA_SERVER = "10.0.1.39"
+OTA_USER   = "chof"
+OTA_PATH   = "/var/data/homecenter/firmware"
